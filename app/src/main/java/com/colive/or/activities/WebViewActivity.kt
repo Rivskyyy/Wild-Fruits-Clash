66913ab5974c5d4ae1635a09abc22f580fd2d5a3
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2022-12-02 16:30:20 +0200

    fixed onPageFinished

--- app/src/main/java/com/colive/or/activities/WebViewActivity.kt
+++ app/src/main/java/com/colive/or/activities/WebViewActivity.kt
@@ -18,18 +18,16 @@ import com.colive.or.App
 import com.colive.or.data.FileCreatorFruitsClash
 import com.colive.or.R
 import com.google.android.material.snackbar.Snackbar
-
+const val BASE_LINK = "https://wildfruitsclash.today/"
 //const val userAgent= "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.5359.61 Mobile Safari/537.36"
 class WebViewActivity : AppCompatActivity() {
     private lateinit var webView: WebView
 //    val REQUEST_SELECT_FILE = 100
 //    private val FILECHOOSER_RESULTCODE = 1
    private var uploadMessage: ValueCallback<Array<Uri>>? = null
-//    private var mUploadMessage: ValueCallback<*>? = null
     val getContent =   registerForActivityResult(ActivityResultContracts.GetMultipleContents()) {
         uploadMessage?.onReceiveValue(it.toTypedArray())
     }
-
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,7 +36,7 @@ class WebViewActivity : AppCompatActivity() {
 
         webView = findViewById(R.id.webView)
 
-
+   //webView.loadUrl("wildfruitsclash.today/")
 
         CookieManager.getInstance().setAcceptCookie(true)
         CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true)
@@ -57,8 +55,9 @@ class WebViewActivity : AppCompatActivity() {
 //       webView.settings.userAgentString.
        val userAgent =  WebView(this).settings.userAgentString.replace("wv", "")
           webView.settings.userAgentString = userAgent
+    val newUrl =  FileCreatorFruitsClash.BASE.substringBefore("/")
 
-        Log.d("user_agent", userAgent.toString() )
+       Log.d("newUrl", newUrl.toString() )
 
         webView.settings.loadWithOverviewMode = false
        // webView.settings.userAgentString.replace("wv", "")
@@ -69,8 +68,6 @@ class WebViewActivity : AppCompatActivity() {
 
         webView.settings.allowContentAccess = true
         webView.settings.allowFileAccess = true
-
-
 
         webView.webViewClient = object : WebViewClient() {
             override fun onReceivedError(
@@ -85,12 +82,20 @@ class WebViewActivity : AppCompatActivity() {
             override fun onPageFinished(view: WebView, url: String) {
                 super.onPageFinished(view, url)
                 CookieManager.getInstance().flush()
-                if (FileCreatorFruitsClash.BASE.substringBefore("/") == url) {
-                    startActivity(Intent(this@WebViewActivity, App::class.java))
+
+//                FileCreatorFruitsClash.BASE.substringBefore("/") == url
+                    // BASE_LINK = "https://wildfruitsclash.today/"
+
+                if  (BASE_LINK == url) {
+
+                    startActivity(Intent(this@WebViewActivity, CreditsScreen::class.java))
+                    finish()
                 } else {
-                    (application as App).file.writeData(url)
+                    Log.d("base_url", "FALSE")
+                 val url =  (application as App).file.writeData(url)
                 }
             }
+
         }
         webView.webChromeClient = object : WebChromeClient() {
 
@@ -160,8 +165,6 @@ class WebViewActivity : AppCompatActivity() {
                     if (webView.canGoBack()) {
                         webView.goBack()
                     }
-                     
-
                 }
             })
     }
